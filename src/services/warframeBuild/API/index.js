dfd96d956e5d58b6c1b3fa7edce138e7fd50a968
Siwat-Keng <siwat.keng.pongpanit@gmail.com>
2021-07-10 12:36:48 +0700

    fixed build

--- src/services/warframeBuild/API/index.js
+++ src/services/warframeBuild/API/index.js
@@ -2,25 +2,28 @@ import fetch from 'node-fetch'
 import stringSimilarity from 'string-similarity'
 
 const BASE_URL = 'https://overframe.gg/api/v1/'
+const CATEGORY = ['Suits', 'LongGuns', 'Melee', 'Pistols', 'SpaceSuits', 'Sentinels', 'MoaPets']
 
 const getItemByName = name =>
     fetch(`${BASE_URL}items?limit=9999`)
         .then(response => response.json())
         .then(json => {
-            var bestMatchItem
-            var bestMatchScore = -Infinity
-            json.results.map(item => {
-                if (
-                    stringSimilarity.compareTwoStrings(item.name, name.toUpperCase()) >
-          bestMatchScore
-                ) {
-                    bestMatchItem = item
-                    bestMatchScore = stringSimilarity.compareTwoStrings(
-                        item.name,
-                        name.toUpperCase(),
-                    )
-                }
-            })
+            let bestMatchItem
+            let bestMatchScore = 0.3
+            json
+                .results
+                .filter(item => CATEGORY.includes(item.category))
+                .map(item => {
+                    if (
+                        stringSimilarity.compareTwoStrings(item.name, name.toUpperCase()) > bestMatchScore
+                    ) {
+                        bestMatchItem = item
+                        bestMatchScore = stringSimilarity.compareTwoStrings(
+                            item.name,
+                            name.toUpperCase(),
+                        )
+                    }
+                })
             return bestMatchItem
         })
 
@@ -29,13 +32,13 @@ const fetchBuild = name =>
         fetch(`${BASE_URL}builds?item_id=${item.id}&sort_by=Score`)
             .then(response => response.json())
             .then(json => json.results.slice(0, 5)),
-    )
+    ).catch(() => [])
 
 const fetchDefaultBuild = name =>
     getItemByName(name).then(item =>
         fetch(`${BASE_URL}builds?item_id=${item.id}&author_id=10027`)
             .then(response => response.json())
             .then(json => json.results),
-    )
+    ).catch(() => [])
 
 module.exports = { fetchBuild, fetchDefaultBuild }
